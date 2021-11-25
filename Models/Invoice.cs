861ef145d38dc98bcd5e901576ea0dbd34d1cf69
A00218636 <84596063+A00218636@users.noreply.github.com>
2021-11-24 23:21:51 -0800

    Add project files.

---
+++ Models/Invoice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillTracker.Models
+{
+    public class Invoice
+    {
+        [Key]
+        int InvoiceNumber { get; set; }
+
+        int TerminanlID { get; set; }
+
+        string ProcessedBy { get; set; }
+
+        Product Product { get; set; }
+
+        string ProdDesc { get; set; }
+
+        double SellingPrice { get; set; }
+
+        DateTime InvcDateTime { get; set; }
+
+        List<ItemList> provinceList = new List<ItemList>()
+        {
+        new ItemList { Text = "Alberta", Value = 1 },
+        new ItemList { Text = "BC", Value = 2 },
+        new ItemList { Text = "Manitoba", Value = 3 },
+        new ItemList { Text = "NB", Value = 4 },
+        new ItemList { Text = "Ontario", Value = 5 },
+        };
+
+
+
+        double GST { get; set; }
+
+        double HST { get; set; }
+
+        double PST { get; set; }
+
+        double TotalTax { get; set; }
+
+        string Notes { get; set; }
+
+        double SubTotal { get; set; }
+
+        double Total { get; set; }
+
+        int GSTHSTRefNo { get; set; }
+
+        string CustomerInstructions { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BillTracker.Models
+{
+    public class Product
+    {
+        [Key]
+        string ID { get; set; }
+
+        string Description { get; set; }
+
+        Category ProdCategory { get; set; }
+
+        private class Category
+        {
+            [Key]
+            string ID { get; set; }
+            string Description { get; set; }
+        }
+
+       double Discount { get; set; }
+
+
+    }
+}
